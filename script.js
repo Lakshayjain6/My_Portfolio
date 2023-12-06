c8d5be601d04715f86f15c7f88dcdc34ce0c0fdb
Lakshayjain6 <94297565+Lakshayjain6@users.noreply.github.com>
2023-12-06 23:16:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Forntend Developer", "Software Developer"],
+        strings: ["Forntend Developer", "Software Developer"," Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Forntend Developer", "Software Developer"],
+        strings: ["Forntend Developer", "Software Developer","Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
